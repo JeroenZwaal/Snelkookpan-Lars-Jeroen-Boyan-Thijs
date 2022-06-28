3b3f1b49a3fc556059033e02e327546315eadcc0
ThijsLowensteijn <thijslowensteijn05@gmail.com>
2022-06-28 13:37:41 +0200

    header.php gemaakt
    fix #10

---
+++ header.php
@@ -0,0 +1,9 @@
+<header>
+		<h1>Snelkookpan</h1>
+		<nav>
+			<a href = "drinks.html">Dranken</a>
+			<a href = "houses.php">Huizen</a>
+            <a href = "drinks.html">Dranken</a>
+			<a href = "inlog.php">Login</a>
+		</nav>
+</header>
