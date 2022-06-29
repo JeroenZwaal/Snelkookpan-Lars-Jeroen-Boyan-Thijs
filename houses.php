7e29fbe9d5b4bdbb435f1f30ac73f55af7879fe5
Lars-van-Bezooijen <latsvb10@gmail.com>
2022-06-29 13:21:45 +0200

    updated houses.php with filter and style

--- houses.php
+++ houses.php
@@ -10,10 +10,26 @@
 </head>
 
 <body>
+    <?php require_once 'header.php';?>
     <main class="houses">
         <div class="filter">
             <p>Filter opties</p>
             <form action="">
+                <div class="form-group">
+                    <label for="location">Locatie</label>
+                    <input class="input" type="text" name="location" id="location" placeholder="locatie">
+                </div>
+                <div class="form-group">
+                    <label for="rooms">Kamers</label>
+                    <input class="input" type="number" name="rooms" id="rooms" placeholder="kamers">
+                </div>
+                <div class="form-group">
+                    <label for="price">Prijs</label>
+                    <div>
+                        <input class="price-input" type="number" name="price_min" id="price_min" placeholder="min">
+                        <input class="price-input" type="number" name="price_max" id="price_max" placeholder="max">
+                    </div>
+                </div>
                 
             </form>
 
@@ -36,13 +52,13 @@
                         <div class="house-info-column">
                             <p><?php echo $house['zipcode']; ?> <?php echo $house['streetname']; ?></p>
                             <p><?php echo $house['area']; ?></p>
-                            <p>&euro;<?php echo number_format($house['price'], 0, ",", "."); ?></p>
+                            <p>&euro;<?php echo number_format($house['price'], 0, ",", ".") . "/week"; ?></p>
                         </div>
                         <div class="house-info-column">
                             <a class="reserve" href="">Reserveer</a>
                         </div>
                     </div>
-                    
+                    <p><?php echo $house['description']; ?></p>
                 </div>
             <?php
             }
