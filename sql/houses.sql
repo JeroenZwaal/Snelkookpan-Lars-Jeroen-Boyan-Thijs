c3f002b4ebb808edd2333be5d627a21c6cd3ecae
Lars-van-Bezooijen <latsvb10@gmail.com>
2022-06-29 13:17:28 +0200

    added house rooms row

--- sql/houses.sql
+++ sql/houses.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Gegenereerd op: 29 jun 2022 om 13:12
+-- Gegenereerd op: 29 jun 2022 om 13:16
 -- Serverversie: 10.4.24-MariaDB
 -- PHP-versie: 8.1.5
 
@@ -35,6 +35,7 @@ CREATE TABLE `houses` (
   `description` varchar(255) NOT NULL,
   `area` varchar(255) NOT NULL,
   `zipcode` varchar(255) NOT NULL,
+  `rooms` int(11) NOT NULL,
   `status` int(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -42,12 +43,12 @@ CREATE TABLE `houses` (
 -- Gegevens worden geëxporteerd voor tabel `houses`
 --
 
-INSERT INTO `houses` (`id`, `streetname`, `image`, `price`, `description`, `area`, `zipcode`, `status`) VALUES
-(1, '\'t Hooft 76', 'img/houses/house1.jpg', 3500, 'Wat een ruime woning, deze tweekapper met uitbouw aan de achterzijde en aangebouwde garage gelegen in de Drait.', 'Moerdijk', '4859 KA', 0),
-(2, 'Overlaat 10', 'img/houses/house2.jpg', 5500, 'Op een uitzonderlijk mooie plek midden in de natuur, op een rustieke locatie aan de Overlaat te Gassel, staat dit in de jaren 80 onder architectuur gebouwde landhuis met uitzicht over het natuurgebied van de Kraaijenbergse Plassen.', 'Tholen', '3462 AR', 0),
-(3, 'Linde 27', 'img/houses/house3.jpg', 6000, 'Met een perceeloppervlakte van 230 m2, maar liefst een totale woonoppervlakte van 173 m² met een woonkamer/keuken van 57 m², een inhoud van 642 m³ en een tuin van 100 m² kun je alle kanten op.', 'Waalwijk', '4495 UR', 1),
-(4, 'Linde 27', 'img/houses/house4.jpg', 4500, 'Royale en sfeervolle half vrijstaande woning, gelegen in het centrum van Roosendaal. Deze woning met veel lichtinval heeft 4 royale slaapkamers, een gezellige achtertuin op het westen met achterom en een stenen berging.\r\n', 'Breda', '2954 UB', 0),
-(5, 'Linde 27', 'img/houses/house5.jpg', 6000, 'Deze fraaie, vrijstaande woning is gelegen in een landelijke omgeving in het prachtige Vriezenveen. De woning is onder architectuur gebouwd (Bekhuis & Kleinjan architecten). De woning beschikt over een statige oprijlaan die wordt afgesloten door een ijzer', 'Breda', '2934 UB', 0);
+INSERT INTO `houses` (`id`, `streetname`, `image`, `price`, `description`, `area`, `zipcode`, `rooms`, `status`) VALUES
+(1, '\'t Hooft 76', 'img/houses/house1.jpg', 3500, 'Wat een ruime woning, deze tweekapper met uitbouw aan de achterzijde en aangebouwde garage gelegen in de Drait.', 'Moerdijk', '4859 KA', 5, 0),
+(2, 'Overlaat 10', 'img/houses/house2.jpg', 5500, 'Op een uitzonderlijk mooie plek midden in de natuur, op een rustieke locatie aan de Overlaat te Gassel, staat dit in de jaren 80 onder architectuur gebouwde landhuis met uitzicht over het natuurgebied van de Kraaijenbergse Plassen.', 'Tholen', '3462 AR', 6, 0),
+(3, 'Linde 27', 'img/houses/house3.jpg', 6000, 'Met een perceeloppervlakte van 230 m2, maar liefst een totale woonoppervlakte van 173 m² met een woonkamer/keuken van 57 m², een inhoud van 642 m³ en een tuin van 100 m² kun je alle kanten op.', 'Waalwijk', '4495 UR', 4, 1),
+(4, 'Linde 27', 'img/houses/house4.jpg', 4500, 'Royale en sfeervolle half vrijstaande woning, gelegen in het centrum van Roosendaal. Deze woning met veel lichtinval heeft 4 royale slaapkamers, een gezellige achtertuin op het westen met achterom en een stenen berging.\r\n', 'Breda', '2954 UB', 5, 0),
+(5, 'Linde 27', 'img/houses/house5.jpg', 6000, 'Deze fraaie, vrijstaande woning is gelegen in een landelijke omgeving in het prachtige Vriezenveen. De woning is onder architectuur gebouwd (Bekhuis & Kleinjan architecten). De woning beschikt over een statige oprijlaan die wordt afgesloten door een ijzer', 'Breda', '2934 UB', 5, 0);
 
 --
 -- Indexen voor geëxporteerde tabellen
