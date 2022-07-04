1e5d6bb238fa07d2bbee0870f42d3b0240771cd1
Lars-van-Bezooijen <latsvb10@gmail.com>
2022-07-04 09:50:10 +0200

    fixed styling general.php

--- general.php
+++ general.php
@@ -1,17 +1,41 @@
-<?php require_once 'header.php'?>
+<!doctype html>
+<html class="no-js" lang="">
 
-<h1>Algemene Informatie</h1>
-<h2>Over ons</h2>
-<p>Wij bij snelkookpan BV verhuur huizen voor een bepaalde periode.</p>
-<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis dicta, reprehenderit odio nesciunt, alias unde, in perspiciatis laboriosam ratione nihil incidunt tempora porro repellat omnis nisi? Doloremque corporis libero error!
-Asperiores sequi ea accusantium. Ipsam ratione optio beatae debitis ut tempore quos sed voluptas corporis accusantium? Iste quia necessitatibus mollitia magni suscipit aliquid modi iure ipsam, odit nam ex blanditiis.
-Non, quia quasi? Explicabo, repudiandae adipisci. Nemo consectetur, dolore ducimus earum harum ea explicabo optio omnis. Perferendis minima in ut earum optio deleniti? Aspernatur, reprehenderit incidunt nobis vero ut similique.
-Inventore voluptates sapiente at repellendus soluta magni ea tempore nobis possimus obcaecati delectus voluptatum quibusdam, dicta rem illo blanditiis tempora officiis numquam, natus cum, non nam sunt? Enim, quae ipsam?
-Doloremque qui quod nobis amet! Sint perspiciatis doloribus dolorem illum ea id maiores animi perferendis inventore, omnis earum velit sapiente maxime ab, consequuntur obcaecati reprehenderit ipsa ex aspernatur! Ad, perspiciatis.
-Qui ad non doloremque veniam quisquam atque nostrum illum iusto modi odio blanditiis totam nihil excepturi soluta in voluptates, eius aliquam consequuntur obcaecati ratione aliquid cum! Ratione placeat eligendi alias!
-Minus, magnam. Corporis exercitationem recusandae ipsum similique voluptates non a quos aut, voluptate nemo harum sed! Ipsam impedit quod unde, molestiae eligendi at. Iste veritatis itaque porro nesciunt, dolorum architecto!
-Numquam impedit dicta repellat voluptatum molestias eos ratione amet a atque sunt ullam consectetur iste iusto in ab asperiores quidem eius quibusdam id ut labore, hic nulla. Ducimus, exercitationem possimus!
-Itaque, vel quis tenetur, natus animi ipsum pariatur amet repellat aspernatur nisi, quidem nulla quo explicabo iste dicta beatae eligendi tempora quasi possimus perspiciatis obcaecati quas quam quod! Quidem, quae?
-Veritatis id beatae esse saepe nam expedita dignissimos vitae animi soluta atque eius deserunt itaque laboriosam ab, facilis ipsam totam, ullam facere dolorum nulla in molestias dolor vel perspiciatis. Veniam!</p>
+<head>
+  <meta charset="utf-8">
+  <title></title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<?php require_once 'footer.php'?>
+  <meta property="og:title" content="">
+  <meta property="og:type" content="">
+  <meta property="og:url" content="">
+  <meta property="og:image" content="">
+
+  <link rel="manifest" href="site.webmanifest">
+  <link rel="apple-touch-icon" href="icon.png">
+  <!-- Place favicon.ico in the root directory -->
+
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/main.css">
+
+  <meta name="theme-color" content="#fafafa">
+</head>
+<body>
+    <?php require_once 'header.php'?>
+    <h1>Algemene Informatie</h1>
+    <h2>Over ons</h2>
+    <p>Wij bij snelkookpan BV verhuur huizen voor een bepaalde periode.</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis dicta, reprehenderit odio nesciunt, alias unde, in perspiciatis laboriosam ratione nihil incidunt tempora porro repellat omnis nisi? Doloremque corporis libero error!
+    Asperiores sequi ea accusantium. Ipsam ratione optio beatae debitis ut tempore quos sed voluptas corporis accusantium? Iste quia necessitatibus mollitia magni suscipit aliquid modi iure ipsam, odit nam ex blanditiis.
+    Non, quia quasi? Explicabo, repudiandae adipisci. Nemo consectetur, dolore ducimus earum harum ea explicabo optio omnis. Perferendis minima in ut earum optio deleniti? Aspernatur, reprehenderit incidunt nobis vero ut similique.
+    Inventore voluptates sapiente at repellendus soluta magni ea tempore nobis possimus obcaecati delectus voluptatum quibusdam, dicta rem illo blanditiis tempora officiis numquam, natus cum, non nam sunt? Enim, quae ipsam?
+    Doloremque qui quod nobis amet! Sint perspiciatis doloribus dolorem illum ea id maiores animi perferendis inventore, omnis earum velit sapiente maxime ab, consequuntur obcaecati reprehenderit ipsa ex aspernatur! Ad, perspiciatis.
+    Qui ad non doloremque veniam quisquam atque nostrum illum iusto modi odio blanditiis totam nihil excepturi soluta in voluptates, eius aliquam consequuntur obcaecati ratione aliquid cum! Ratione placeat eligendi alias!
+    Minus, magnam. Corporis exercitationem recusandae ipsum similique voluptates non a quos aut, voluptate nemo harum sed! Ipsam impedit quod unde, molestiae eligendi at. Iste veritatis itaque porro nesciunt, dolorum architecto!
+    Numquam impedit dicta repellat voluptatum molestias eos ratione amet a atque sunt ullam consectetur iste iusto in ab asperiores quidem eius quibusdam id ut labore, hic nulla. Ducimus, exercitationem possimus!
+    Itaque, vel quis tenetur, natus animi ipsum pariatur amet repellat aspernatur nisi, quidem nulla quo explicabo iste dicta beatae eligendi tempora quasi possimus perspiciatis obcaecati quas quam quod! Quidem, quae?
+    Veritatis id beatae esse saepe nam expedita dignissimos vitae animi soluta atque eius deserunt itaque laboriosam ab, facilis ipsam totam, ullam facere dolorum nulla in molestias dolor vel perspiciatis. Veniam!</p>
+
+    <?php require_once 'footer.php'?>
+</body>
