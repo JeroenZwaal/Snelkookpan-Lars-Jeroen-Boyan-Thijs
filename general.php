56570dfc8868b9dc35533a4ce61b9f8c89ae113d
ThijsLowensteijn <thijslowensteijn05@gmail.com>
2022-07-04 09:42:29 +0200

    Update general.php

---
+++ general.php
@@ -0,0 +1,8 @@
+<?php require_once 'header.php'?>
+
+<h1>Algemene Informatie</h1>
+<h2>Over ons</h2>
+<p>Wij bij snelkookpan BV verhuur huizen voor een bepaalde periode.</p>
+<p></p>
+
+<?php require_once 'footer.php'?>
